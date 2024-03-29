8b04ca854d52935dae94c28090c7aa671d523ed4
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-03-28 22:35:33 -0400

    15 loops

---
+++ namespace 15Loops.cs
@@ -0,0 +1,74 @@
+namespace 15Loops
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i});
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.Writeline({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0; i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+            for(int i = 0: i < 5; i++)
+            {
+            Console.WriteLine({i})
+            }
+
+        }
+
+    }
+
+
+}
